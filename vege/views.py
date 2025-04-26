510b16c36dbbd3bbb6e7102de27d19ba2d3ad1b0
Pallavi kumari <kumaripallavi09375@gmail.com>
2025-04-26 12:09:41 +0530

    Initial commit of Django Receipe project

--- vege/views.py
+++ vege/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,redirect
 from .models import *
 from django.http import HttpResponse
 # Create your views here.
-
-
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.decorators import login_required
+@login_required(login_url='/login/')
 def receipes(request):
    if request.method == "POST":
       data = request.POST
@@ -56,3 +59,55 @@ def delete_receipe(request, id):
     queryset = Receipe.objects.get(id = id)
     queryset.delete()
     return redirect('/receipes/')
+
+def login_page(request):
+     if request.method == "POST":
+          username = request.POST.get('username')
+          password = request.POST.get('password')
+
+          if not User.objects.filter(username = username).exists():
+             messages.info(request , ' Invalid Username')
+             return redirect('/login/')
+          
+          user = authenticate(username = username , password = password)
+
+          if user is None:
+             messages.info(request , ' Invalid Password')
+             return redirect('/login/')
+     
+          else:
+            login(request ,user)
+            return redirect('/receipes/')
+
+     return render(request , 'login.html')
+def logout_page(request):
+     logout(request)
+     return redirect('/login/')
+
+
+def register(request):
+     if request.method == "POST":
+          first_name = request.POST.get('first_name')
+          last_name = request.POST.get('last_name')
+          username = request.POST.get('username')
+          password = request.POST.get('password')
+
+          user = User.objects.filter(username = username)
+
+          if user.exists():
+               messages.info(request , 'Username already exist')
+               return redirect('/register/')
+
+          user = User.objects.create(
+             first_name  = first_name ,
+             last_name =    last_name ,
+             username = username 
+          )
+          user.set_password(password)
+          user.save()
+          messages.info(request , 'Account created successfully')
+          return redirect('/register/')
+
+
+
+     return render(request , 'register.html')
